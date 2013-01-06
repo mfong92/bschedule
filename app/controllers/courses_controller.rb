a93967bf70645e8c3bdcd82d31f951bb4bc18ef9
mfong92 <mfong92@gmail.com>
2013-01-06 15:35:23 -0800

    center all the things

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -8,7 +8,7 @@ class CoursesController < ApplicationController
     require 'nokogiri'
     require 'open-uri'
     require 'builder'
-    @course = params[:dept] + params[:course_num]
+    @course = params[:dept] + " " + params[:course_num]
   	@title, @info = live_data(params[:dept], params[:course_num])
   end
 end
